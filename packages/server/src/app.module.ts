bbeb8d66d5020a2ba7d90b4100692e3ca834835d
Kevin Setayesh <kevinsetayesh7@gmail.com>
2025-06-18 10:47:19 -0400

    Removed user related code

--- packages/server/src/app.module.ts
+++ packages/server/src/app.module.ts
@@ -1,14 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UsersModule } from './modules/users/users.module';
 import { GeneratescriptModule } from './modules/generatescript/generatescript.module';
 
 @Module({
-    imports: [
-        UsersModule,
-        GeneratescriptModule,
-    ],
+    imports: [GeneratescriptModule],
     controllers: [AppController],
     providers: [AppService],
 })
--- packages/server/src/modules/generatescript/generatescript.module.ts
+++ packages/server/src/modules/generatescript/generatescript.module.ts
@@ -3,8 +3,6 @@ import { Module } from '@nestjs/common';
 import { GeneratescriptController } from './generatescript.controller';
 import { ScriptGeneratorService } from './generatescript.service';
 
-export const BASH_SCRIPT_PATH = 'BASH_SCRIPT_PATH';
-
 @Module({
     controllers: [GeneratescriptController],
     providers: [
